cd903a708c93802574d8226fef3f58a72e840b24
Maulina Sabrina <maulinasabrina04@gmail.com>
2025-12-09 09:07:28 +0700

    fix: add page under maintain

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -9,7 +9,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 
   <!-- Tailwind -->
-  @vite(['resources/css/admin.css'])
+  @vite(['resources/css/admin.css', 'resources/js/admin.js'])
 
  
   @stack('styles')
